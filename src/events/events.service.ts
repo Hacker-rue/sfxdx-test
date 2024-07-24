590704575282bd2d776f0890bb9fecb51dd08d6c
Hacer-rue <pavlik.soldatov.12@gmail.com>
2024-07-24 07:16:56 +0400

    update addCreateOrder

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -9,22 +9,47 @@ import { Query } from 'typeorm/driver/Query';
 @Injectable()
 export class EventsService implements OnModuleInit {
   
+  provider = new ethers.JsonRpcProvider(process.env.INFURA_URL)
+  contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, contractABI, this.provider)
+
   constructor(
     @InjectQueryService(Orders)
     private ordersService: QueryService<Orders>
-  ) {}
-
-  async onModuleInit() {
-    console.log(process.env.INFURA_URL)
-    const provider = new ethers.JsonRpcProvider(process.env.INFURA_URL)
-    const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, contractABI, provider)
-
-    await this.fetchAllEventsSinceDeployment(contract)
-    contract.on('OrderCreated', this.orderCreated)
+  ) {
+    this.contract.on('OrderCreated', this.orderCreated.bind(this))
   }
 
-  private async orderCreated(...args) {
-    console.log()
+  async onModuleInit() {
+    await this.fetchAllEventsSinceDeployment(this.contract)
+  }
+
+  private async orderCreated(...data) {
+    try {
+      const orders = await this.ordersService.query({
+        filter: {OrdersId: {eq: data[0]}}
+      })
+      if(orders[0] === undefined) {
+        this.ordersService.createOne({
+          OrdersId: data[0],
+          AmountA: data[1],
+          AmountB: data[2],
+          TokenA: data[3],
+          TokenB: data[4],
+          UserAddres: data[5],
+          IsActive: true
+        })
+      }
+    } catch {
+      this.ordersService.createOne({
+        OrdersId: data[0],
+        AmountA: data[1],
+        AmountB: data[2],
+        TokenA: data[3],
+        TokenB: data[4],
+        UserAddres: data[5],
+        IsActive: true
+      })
+    }
   }
 
   private async fetchAllEventsSinceDeployment(contract: ethers.Contract) {
